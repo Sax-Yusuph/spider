5ac50ddc4a55622c424fe06077f23b986330e187
Sax <yusuphshamsondeen@gmail.com>
2020-10-30 04:32:00 +0100

    removed testing console logs

--- server.js
+++ server.js
@@ -4,7 +4,6 @@ const format_res = require('./utils/format_res')
 const get_SPA = require('./utils/get_SPA')
 const fetchData = require('./utils/get_store')
 const fs = require('fs')
-const { performance } = require('perf_hooks')
 const bodyParser = require('body-parser')
 
 const app = express()
@@ -14,65 +13,32 @@ let browser
 app.use(bodyParser.json())
 
 app.get('/', (req, res) => {
-	if (browser) return
-	puppeteer
-		.launch({
-			headless: false,
-			args: [
-				// "--proxy-server=" + proxy,
-				'--no-sandbox',
-				'--disable-setuid-sandbox',
-				'--disable-dev-shm-usage',
-				'--disable-accelerated-2d-canvas',
-				'--disable-gpu',
-				'--window-size=1920x1080',
-			],
-		})
-		.then(chromium => {
-			browser = chromium
-			res.send('Chromium Instance Launched')
-		})
+	res.send('ready to test server')
 })
 const defaultUserRequest = {
 	item: 'infinix hot 8',
-	urls: ['Konga', 'Jumia', 'AliExpress', 'Kara', 'Ebay', 'Slot'],
+	urls: ['Slot'],
 }
+
 app.get('/getstore', (req, res) => {
 	const formattedURLs = format_res(defaultUserRequest)
 	// res.send(formattedURLs)
-	const t0 = performance.now()
-	if (browser) {
-		Promise.all([
-			...formattedURLs.urls.map(fetchData),
-			...formattedURLs.SPA_Store_Urls.map(async uri => {
-				return get_SPA(uri, browser)
-			}),
-		])
-			.then(results => {
-				const formatted_results = []
-				results.forEach(result => formatted_results.push(...result))
+	Promise.all([...formattedURLs.urls.map(fetchData)])
+		.then(results => {
+			const formatted_results = []
+			results.forEach(result => formatted_results.push(...result))
 
-				res.send(JSON.stringify(formatted_results, null, 3))
-				fs.writeFile(
-					'serverResults.json',
-					JSON.stringify(formatted_results, null, 3),
-					() => {
-						console.log(
-							`scraped ${formatted_results.length} results >>>>>>>>`
-						)
-						console.log('yay! successfully completed')
-						console.log(
-							`finished in : ${Math.ceil(
-								(performance.now() - t0) / 1000
-							)} seconds`
-						)
-					}
-				)
-			})
-			.catch(e => console.log(e.message))
-	} else {
-		res.status(400).send('Opps! Chromium not launched yet')
-	}
+			res.json(formatted_results)
+			fs.writeFile(
+				'serverResults.json',
+				JSON.stringify(formatted_results, null, 3),
+				() => {
+					console.log(`scraped ${formatted_results.length} results >>>>>>>>`)
+					console.log('yay! successfully completed')
+				}
+			)
+		})
+		.catch(e => console.log(e.message))
 })
 
 const PORT = 4000
